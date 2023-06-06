5670e0c65b0a0fff715a1837a3b2bf0df4d2aa06
olgasolda <olyasis@gmail.com>
2023-06-06 17:56:21 +0300

    Template driven form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,58 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm, ValidationErrors} from "@angular/forms";
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent {
+export class FormComponent implements OnInit, AfterViewInit {
   roles: string[] = ['Гость', 'Модератор', 'Администратор'];
   user = {id: 1, name: '', age: '', role: ''};
 
+  formErrors: any = {
+    name: '',
+    age: ''
+  };
+  validationMessages: any = {
+    name: {
+      required: 'Имя обязательно.',
+      minLength: 'Имя должно содержать минимум 4 символа.'
+    },
+    age: {
+      required: 'Возраст обязателен.'
+    }
+  };
+
+  @ViewChild('userForm') userForm!: NgForm;
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.userForm.valueChanges?.subscribe(() => this.onValueChanged());
+  }
+
   onSubmit(d: any): void {
     console.log(d);
+  }
+
+  private onValueChanged() {
+    const form = this.userForm.form;
+
+    Object.keys(this.formErrors).forEach(fieldName => {
+      const control = form.get(fieldName);
+      this.formErrors[fieldName] = '';
+
+      if (control && control.dirty && control.invalid) {
+        const messages = this.validationMessages[fieldName];
+
+        Object.keys(control.errors as ValidationErrors).forEach(key => {
+          console.log(messages[key]);
+          this.formErrors[fieldName] += messages[key] + '';
+        });
+      }
+    });
   }
 }
